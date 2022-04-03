6fea50c0603434ea43b20d4a76daeacd517e9770
salman1204 <md.salman@northsouth.edu>
2022-04-03 13:00:55 +0600

    3 class based view

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('blog/', views.blog_list),
-    path('blog/<int:pk>/', views.blog_details)
+    path('blog/', views.BlogList.as_view()),
+    path('blog/<int:pk>/', views.BlogDetail.as_view())
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,44 +1,14 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
+from rest_framework import generics
 from blog.models import Blog
 from blog.serializers import BlogSerializer
 
 # Create your views here.
 
-@api_view(['GET', 'POST'])
-def blog_list(request):
-    if request.method == 'GET':
-        blogs = Blog.objects.all()
-        serializer = BlogSerializer(blogs, many =True)
-        return Response(serializer.data)
-    
-    elif request.method == 'POST':
-        serializer = BlogSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class BlogList(generics.ListCreateAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def blog_details(request, pk):
-    try:
-        blog = Blog.objects.get(pk=pk)
-    except Blog.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = BlogSerializer(blog)
-        return Response(serializer.data)
-    
-    elif request.method == "PUT":
-        serializer = BlogSerializer(blog, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == 'DELETE':
-        blog.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class BlogDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
